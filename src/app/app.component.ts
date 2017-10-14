4026f866a203228a9e8f4518257c4cb7e44cc968
dharis <dharris@elevate.com>
2017-10-13 19:55:11 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,18 @@ export class AppComponent implements OnInit {
   dragStartY:number;
   dragEndX:number;
   dragEndY:number;
+  mainCanvas:any;
 
   isDragging:boolean = false;
 
 
   ngOnInit(){
     //this.draw();
+    this.mainCanvas = document.getElementById('canvas') as HTMLCanvasElement;
+    var canvas = this.mainCanvas;
+    var e:any;
+    canvas.onmousedown = this.mouseDown(e);
+    canvas.onmouseup = this.mouseUp(e);
   }
 
   draw() {
@@ -30,7 +36,7 @@ export class AppComponent implements OnInit {
   }
   
   drawRect(x:number, y:number, endX:number, endY:number) {
-    var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    var canvas = this.mainCanvas;
     var canvasBox = canvas.getBoundingClientRect();
     // x = x - canvasBox.left;
     // y = y - canvasBox.top;
@@ -61,7 +67,45 @@ export class AppComponent implements OnInit {
         ctx.strokeRect(x, endY, width, height);
       } else if(wentBackwards){
         ctx.strokeRect(endX, y, width, height);
-      }else{
+      } else {
+        ctx.strokeRect(x, y, width, height);
+      }
+    }
+  }
+  
+  drawCirc(x:number, y:number, endX:number, endY:number) {
+    var canvas = this.mainCanvas;
+    var canvasBox = canvas.getBoundingClientRect();
+    // x = x - canvasBox.left;
+    // y = y - canvasBox.top;
+    // endX = endX - canvasBox.left;
+    // endY = endY - canvasBox.top;
+    var width = Math.abs(x - endX);
+    var height = Math.abs(y - endY);
+    height = height < 3 ? 2 : height;
+    width = width < 3 ? 2 : width;
+    var wentBackwards:boolean = false;
+    var wentUpwards:boolean = false;
+
+    if (x > endX){
+      wentBackwards = true;
+    }
+    if (y > endY){
+      wentUpwards = true;
+    }
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.strokeStyle="#000099";
+      //ctx.strokeRect(x, y, width, height);
+      //debugger;
+      if(wentBackwards && wentUpwards){
+        ctx.str(endX, endY, width, height);
+      } else if(wentUpwards){
+        ctx.strokeRect(x, endY, width, height);
+      } else if(wentBackwards){
+        ctx.strokeRect(endX, y, width, height);
+      } else {
         ctx.strokeRect(x, y, width, height);
       }
     }
